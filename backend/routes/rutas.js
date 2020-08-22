d0afaeb157e14a13643d5ab19e264abde2d702d7
choquesaurus <daniechoque159@gmail.com>
2020-08-22 09:12:08 -0500

    update backend/app.js cors auth.choquesaurus.com v15

--- backend/routes/rutas.js
+++ backend/routes/rutas.js
@@ -10,7 +10,7 @@ router.get('/google',passport.authenticate('google',
  { scope: ["profile", "email"] }));
 
 router.get('/auth/google/redirect',passport.authenticate('google',{
-    successRedirect:'https://auth2.netlify.app',
+    successRedirect:'https://auth2.netlify.app/profile',
     failureRedirect:'/auth/login/failed'
 })); 
 router.get('/auth/login/success',async (req,resp,next)=>{
